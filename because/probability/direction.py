7762d9e03afa92b234c5d844cfce75d1a252b5ba
verlocks <919525286@qq.com>
2022-07-11 13:28:57 -0400

    Add comments for direction test

--- because/probability/direction.py
+++ because/probability/direction.py
@@ -14,7 +14,20 @@ from sklearn.neural_network import MLPRegressor
 from sklearn.neighbors import KNeighborsRegressor
 
 def test_direction(rvA, rvB, power=1, N_train=100000):
-    """
+    """ When having power parameter less than or equal to 1,
+        test the causal direction between variables A and B
+        using one of the LiNGAM or GeNGAM pairwise algorithms.
+
+        When having power larger than 1, use non-linear method
+        to test the causal direction. N_train determines at most
+        how many samples would be used to train the non-linear
+        model. Currently test uses KNN algorithm.
+
+        Returns a number R.  A positive R indicates that the
+        causal path runs from A toward B.  A negative value
+        indicates a causal path from B towards A.  Values
+        close to zero (e.g. +/- 10**-5) means that causal
+        direction could not be determined.
     """
     if power <= 1:
         # Pairwise Lingam Algorithm (Hyperbolic Tangent (HT) variant)
@@ -59,29 +72,3 @@ def non_linear_direct_test(A, B, N_train=100000):
     residual = s2 - reg.predict(s1)
     (p, Sta) = RCoT(A, residual)
     return p[0]
-
-'''
-def test_direction(rvA, rvB):
-    """ Test the causal direction between variables A and B
-        using one of the LiNGAM or GeNGAM pairwise algorithms.
-        Returns a number R.  A positive R indicates that the
-        causal path runs from A toward B.  A negative value
-        indicates a causal path from B towards A.  Values
-        close to zero (e.g. +/- 10**-5) means that causal
-        direction could not be determined.
-    """
-    s1 = standardize(rvA)
-    s2 = standardize(rvB)
-    # Pairwise Lingam Algorithm (Hyperbolic Tangent (HT) variant)
-    cum = 0
-    for i in range(len(s1)):
-        v1 = s1[i]
-        v2 = s2[i]
-        cumulant = v1*math.tanh(v2) - v2*math.tanh(v1)
-        cum += cumulant
-    avg = cum / float(len(s1))
-    cc = np.corrcoef([s1,s2])
-    rho = cc[1,0]
-    R = rho * avg
-    return R
-'''
